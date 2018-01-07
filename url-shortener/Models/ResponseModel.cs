4711d9edba74e1bb74666eb6004712736bfd2c03
Nate Jenson <nate.m.jenson@gmail.com>
2018-01-07 14:45:27 -0600

    create response model

---
+++ url-shortener/Models/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace url_shortener.Models
+{
+    public class ResponseModel
+    {
+        int StatusCode { get; set; }
+        dynamic Data { get; set; }
+        string Message { get; set; }
+    }
+}
