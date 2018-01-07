45b52299ae3090b7e6579509df9d28dfb7562016
Nate Jenson <nate.m.jenson@gmail.com>
2018-01-07 14:02:31 -0600

    Add project files.

---
+++ url-shortener/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace url_shortener.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
