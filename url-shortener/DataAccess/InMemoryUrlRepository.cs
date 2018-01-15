13faf4311509ecf55c6979f6f28d5259acb64d43
Nate Jenson <nate.m.jenson@gmail.com>
2018-01-14 21:02:10 -0600

    create in-memory url repository

---
+++ url-shortener/DataAccess/InMemoryUrlRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace url_shortener.DataAccess
+{
+    public class InMemoryUrlRepository : IUrlRepository
+    {
+        private ConcurrentDictionary<string, string> repo;
+
+        public InMemoryUrlRepository()
+        {
+            this.repo = new ConcurrentDictionary<string, string>();
+        }
+
+        public bool Get(Uri url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Uri original, Uri newUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
