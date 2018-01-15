54df86680be306a208ca9a4511670e366b7f1f01
Nate Jenson <nate.m.jenson@gmail.com>
2018-01-14 20:57:14 -0600

    add service layer

---
+++ url-shortener/Services/UrlService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace url_shortener.Services
+{
+    public class UrlService
+    {
+    }
+}
