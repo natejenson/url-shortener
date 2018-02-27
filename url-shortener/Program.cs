bd20523cac91c5e2c01456ee004471dd8f818e5c
Nate Jenson <nate.m.jenson@gmail.com>
2018-02-26 18:59:22 -0600

    add application insights

--- url-shortener/Program.cs
+++ url-shortener/Program.cs
@@ -19,6 +19,7 @@ namespace url_shortener
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
