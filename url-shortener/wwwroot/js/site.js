f09876d68d37716f6ceadce33ea203cf377e7657
Nate Jenson <nate.m.jenson@gmail.com>
2018-03-06 19:57:06 -0600

    add copy button for shortened url

--- url-shortener/wwwroot/js/site.js
+++ url-shortener/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿// Write your JavaScript code.
+﻿
+$(function () {
+    $('#copy-button').click(function () {
+        copyHrefToClipboard($('#short-url'));
+    });
+});
+
+function copyHrefToClipboard(element) {
+    var $temp = $('<input>');
+    $('body').append($temp);
+    $temp.val($(element).attr('href')).select();
+    document.execCommand('copy');
+    $temp.remove();
+}
